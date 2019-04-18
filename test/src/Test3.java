104adf16e20d56f7b72c4d9352ac2a24f8713fdc
hwwhwww <1907375750@qq.com>
2019-04-18 14:13:05 +0800

    Test3测试111

---
+++ test/src/Test3.java
@@ -0,0 +1,9 @@
+public class Test3 {
+    /**
+     * 红色代表新建add完以后编程绿色，蓝色代表修改commit
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("继续来！！！");
+    }
+}
