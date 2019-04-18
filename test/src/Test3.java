f2c81c868b09b749f4fb7d140f94c6d66a64e582
hwwhwww <1907375750@qq.com>
2019-04-18 14:22:00 +0800

    Test测试111

--- test/src/Test3.java
+++ test/src/Test3.java
@@ -1,6 +1,7 @@
 public class Test3 {
     /**
-     * 红色代表新建add完以后编程绿色，蓝色代表修改commit
+     * 红色代表新建add完以后变为绿色，在进行commit push上传，蓝色代表修改commit push上传
+     * 箭头蓝色pull更新最新的代码，push上传
      * @param args
      */
     public static void main(String[] args) {
