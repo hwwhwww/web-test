701fdb4931ac91065da6c366bfb9b7ff2d4b45a0
A11111111111A <1907375750@qq.com>
2019-07-11 21:08:08 +0800

    今天测试一下,代码的修改

--- test/src/Test2.java
+++ test/src/Test2.java
@@ -2,4 +2,13 @@ public class Test2 {
     public  void getInfo(){
         System.out.println("123");
     }
+
+    public void getInfo2(){
+        System.out.println("123123213");
+    }
+
+
+    public void getInfo3(){
+        System.out.println("getInfo3");
+    }
 }
