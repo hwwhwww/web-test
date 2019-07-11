3b96d53681b58fcf1b8a25d0b16c2af5b3770737
A11111111111A <1907375750@qq.com>
2019-07-11 21:31:25 +0800

    Merge branch 'master' of G:\mulu\web-test with conflicts.

--- test/src/Test2.java
+++ test/src/Test2.java
@@ -3,10 +3,16 @@ public class Test2 {
         System.out.println("123");
     }
 
-
     public void getInfo2(){
         System.out.println("123123213");
     }
 
 
+    public void getInfo4(){
+        System.out.println("getInfo3");
+    }
+
+    public void printlnee(){
+        System.out.println("getInfo3");
+    }
 }
