0ea6280516c2d7b3a88b5bb5de6398c6194fd5b4
A11111111111A <1907375750@qq.com>
2019-07-14 19:05:45 +0800

    继续测试

--- test/src/Test2.java
+++ test/src/Test2.java
@@ -1,10 +1,9 @@
 public class Test2 {
     public  void getInfo(){
-        System.out.println("123");
+        System.out.println("123哒哒哒哒哒哒多多多多多多多多多多多多多多多多");
     }
 
-
-    public void getInfo1(){
+    public void getInfo3(){
         System.out.println("3434");
     }
 
